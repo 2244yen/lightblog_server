b3f547b39cb6b1a271331ade0ff0b68e45730097
linhdang <linhdtt92@gmail.com>
2018-12-14 09:30:44 +0700

    update mongo config

--- mongo.js
+++ mongo.js
@@ -5,7 +5,6 @@ const config = require('./config/mongo')[env];
 module.exports = () => {
   var envUrl = process.env[config.use_env_variable]
   var localUrl = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds117834.mlab.com:17834/lightblog`
-  // var localUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_SERVER}`
   var mongoUrl = envUrl ? envUrl : localUrl
   const serverOptions = {
     useNewUrlParser: true,
@@ -14,6 +13,7 @@ module.exports = () => {
     reconnectTries: Number.MAX_VALUE,
     bufferMaxEntries: 0
   }
+  console.log(mongoUrl)
   return mongoose.connect(mongoUrl, serverOptions)
   .then(() => {
     console.log('Database connection successful')
