8d50d79b2d7d8df7de77a65024ae19a6dea07c44
linhdang <linhdtt92@gmail.com>
2018-12-17 10:14:01 +0700

    update code

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -11,19 +11,22 @@ module.exports = {
     body.tags = JSON.parse(body.tags)
     if (req.files.thumbnail) {
       cloudinary.uploader.upload(req.files.thumbnail.path, (result) => {
-        body.thumbnail = result.url || ''
+        const linkImg = result.url.replace('http://res.cloudinary.com/linhdang92/image/upload', '')
+        body.thumbnail = 'http://res.cloudinary.com/linhdang92/image/upload/c_fill,h_350,w_690' + linkImg
         saveArticle(body)
       },{
         resource_type: 'image'
       })
     } else {
-      saveArticle(body) 
+      saveArticle(body)
     }
+    
     function saveArticle (obj) {
-      new Article(obj).save((err, article) => {
-        if (err)
-          return res.status(500).send({ "message": err })
-        return res.status(201).send({ "message": "success", "data": article })
+      const promise = new Article(obj).save()
+      promise.then(article => {
+        res.status(200).send({ "message": "success" })
+      }).catch(err => {
+        return next(err)
       })
     }
   },
@@ -31,37 +34,42 @@ module.exports = {
     let { query } = req
     let data = {}
     const queryData = Article.find()
+    queryData.populate('author')
     if (query.isFeatured) {
-      queryData.where('likes').equals(0)
+      queryData.where('likes').gt(0)
     }
     if (query.search) {
-      queryData.or[{ title: query.search }, { description: query.search }]
+      queryData.or([{ "title": { $regex: '.*' + query.search + '.*' } }, { "description": { $regex: '.*' + query.search + '.*' } }])
     }
-    queryData.limit(10).sort({ createdAt: -1 })
+    if (query.tags) {
+      queryData.where({ "tags": { $elemMatch: { $eq: query.tags }}})
+    }
+    const page = query.page || 1
+    const limit = query.limit || 10
+    queryData.limit(limit)
+    queryData.skip(page > 0 ? (page - 1) * limit : 0)
+    queryData.sort({ createdAt: -1 })
     queryData.exec(function (err, data) {
-      if (err) { 
-        res.status(404).json({"error": "not found","err": err})
-        return
+      if (err) return next(err)
+      if (!data) return next(new Error('No article found.'))
+      const countQuery = Article.find()
+      if (query.search) {
+        countQuery.or([{ "title": { $regex: '.*' + query.search + '.*' } },{ "description": { $regex: '.*' + query.search + '.*' } }])
       }
-      res.status(200).send({ "message": "success", "data": data })
-    })
-  },
-  getTags: (req, res, next) => {
-    const query = Article.find()
-    query.exec(function (err, data) {
-      console.log('tags', err)
-      if (err) { 
-        res.status(404).json({"error": "not found","err": err})
-        return
+      if (query.tags) {
+        countQuery.where({ "tags": { $elemMatch: { $eq: query.tags }}})
       }
-      res.status(200).send({ "message": "success", "data": data })
+      countQuery.countDocuments(function(err, count) {
+        res.status(200).send({ "message": "success", "data": data, "total": count })
+      })
     })
   },
   getDetail: (req, res, next) => {
-    Article.findOne({ url: req.params.id }).then(response => {
-      res.status(200).send({ "message": "success", "data": response })
-    }).catch(err => {
-      res.status(500).send({ "message": "fail", "err": err })
+    Article.findOne({ url: req.params.id })
+    .populate('author')
+    .exec(function (err, data) {
+      if (err) return res.status(500).send({ "message": "fail", "err": err })
+      res.status(200).send({ "message": "success", "data": data }) 
     })
   },
   delete: (req, res, next) => {
@@ -87,6 +95,30 @@ module.exports = {
           res.status(200).send({ "message": "success", "data": "Update data successfully!" })
         })
       }
+    })
+  },
+  clap: (req, res, next) => {
+    Article.findOne({ url: req.params.id }).then(response => {
+      response.clap().then(() => {
+        return res.json({ "message": "Done!" })
+      })
+    }).catch(err => {
+      res.status(500).send({ "message": "fail", "err": err })
+    })
+  },
+  getRelated: (req, res, next) => {
+    const { query, params } = req
+    Article.findOne({ url: params.id }, function(err, data) {
+      if (err) return next(err)
+      const relatedQuery = Article.find()
+      relatedQuery.where({ "_id": { $nin: [data._id] }})
+      relatedQuery.and([{ "tags": { $all: data.tags }}, { "author": data.author._id }])
+      relatedQuery.sort({ createdAt: -1 })
+      relatedQuery.limit(3)
+      relatedQuery.exec(function (err, response) {
+        if (err) return res.status(500).send({ "message": "fail", "err": err })
+        res.status(200).send({ "message": "success", "data": response })
+      })
     })
   }
 }
